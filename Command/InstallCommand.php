33a17054b8640bdf52d32689983a0bb789fedf3b
Arek Jaskólski <arek.jaskolski@gmail.com>
2015-05-14 00:24:32 +0200

    Fixed broken commands

--- Command/InstallCommand.php
+++ Command/InstallCommand.php
@@ -2,14 +2,14 @@
 
 namespace Czogori\PgHeroBundle\Command;
 
-use Symfony\Component\Console\Command\Command;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Czogori\PgHeroBundle\PgHero;
 
-class InstallCommand extends Command
+class InstallCommand extends ContainerAwareCommand
 {
     protected function configure()
     {
@@ -21,6 +21,6 @@ class InstallCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        (new PgHero())->install();
+        $this->getContainer()->get('pg_hero')->install();
     }
 }
--- Command/UninstallCommand.php
+++ Command/UninstallCommand.php
@@ -2,14 +2,14 @@
 
 namespace Czogori\PgHeroBundle\Command;
 
-use Symfony\Component\Console\Command\Command;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Czogori\PgHeroBundle\PgHero;
 
-class UninstallCommand extends Command
+class UninstallCommand extends ContainerAwareCommand
 {
     protected function configure()
     {
@@ -21,6 +21,6 @@ class UninstallCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-      echo  (new PgHero())->uninstall();
+        $this->getContainer()->get('pg_hero')->uninstall();
     }
 }
